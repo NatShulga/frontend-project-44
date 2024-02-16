705732fe1f9be818a1d5e6de6c73f9141dc6e2b5
online shop <dove.sh@yandex.ru>
2024-02-16 13:49:51 +0300

    modified file

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import gcdGame from "../src/games/gcd-game";
+import gcdGame from "../src/games/gcd-game.js";
 gcdGame();
 
-import readlineSync from "../src/games/gcd-game";
+import readlineSync from "../src/games/gcd-game.js";
 readlineSync();
