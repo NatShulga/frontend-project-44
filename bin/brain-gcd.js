bfe405ecc0fc2ee14897d8c51f261ace0babb532
NatShulga <113417815+NatShulga@users.noreply.github.com>
2024-02-21 17:19:38 +0300

    Update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import gcdGame from "../src/games/gcd-game.js";
+import gcdGame from "../src/gcd.js";
 gcdGame();
 
-import readlineSync from "../src/games/gcd-game.js";
+import readlineSync from "../src/gcd.js";
 readlineSync;
