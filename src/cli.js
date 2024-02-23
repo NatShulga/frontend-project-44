6961b0c0b8b3160c5e75e1a8770070e747433ea6
NatShulga <113417815+NatShulga@users.noreply.github.com>
2024-02-23 09:46:35 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,8 @@ import readlineSync from "readline-sync";
 export default function greetUser() {
   console.log("Welcome to the Brain Games!");
   const name = readlineSync.question("May I have your name? ");
-  if (name === "John") {
-    console.log("Hello, John!");
+  if (name === "Tirion") {
+    console.log("Hello, Tirion!");
   } else {
     console.log("Hello!");
   }
