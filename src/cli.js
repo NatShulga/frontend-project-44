c8f302ee7ced76aac8272142d50f9cf9857088a4
NatShulga <mrs.flowerfields@gmail.com>
2024-02-27 13:17:59 +0300

    cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,9 +3,6 @@ import readlineSync from "readline-sync";
 export default function greetUser() {
   console.log("Welcome to the Brain Games!");
   const name = readlineSync.question("May I have your name? ");
-  if (name === "Tirion") {
-    console.log("Hello, Tirion!");
-  } else {
-    console.log("Hello!");
-  }
+  console.log(`Hello, ${name}!`);
+  return name;
 };
