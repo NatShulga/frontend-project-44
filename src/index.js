ea637f6ecb229d6029523a8219334ed5caba9cdc
NatShulga <mrs.flowerfields@gmail.com>
2024-03-04 18:26:02 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -7,24 +7,19 @@ const playCalcGame = () => {
   console.log("What is the result of the expression?");
 
   const operators = ["+", "-", "*"];
-  const getRandomOperator = () =>
-    operators[Math.floor(Math.random() * operators.length)];
+  const getRandomOperator = () => operators[Math.floor(Math.random() * operators.length)];
   const getRandomNumber = () => Math.floor(Math.random() * 100);
 
   const calculateExpression = (num1, num2, operator) => {
-    switch (operator) {
-      case "+":
-        return num1 + num2;
-      case "-":
-        return num1 - num2;
-      case "*":
-        return num1 * num2;
-      default:
-        return null;
-    }
+    const operations = {
+      '+': num1 + num2,
+      '-': num1 - num2,
+      '*': num1 * num2
+    };
+    return operations[operator];
   };
 
-  const playRound = () => {
+  function playRound() {
     const num1 = getRandomNumber();
     const num2 = getRandomNumber();
     const operator = getRandomOperator();
@@ -34,16 +29,14 @@ const playCalcGame = () => {
     const userAnswer = readlineSync.question("Your answer: ");
 
     if (Number(userAnswer) !== correctAnswer) {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return false;
     }
 
     console.log("Correct!");
     return true;
-  };
+  }
 
   for (let i = 0; i < 3; i += 1) {
     if (!playRound()) {
@@ -55,5 +48,3 @@ const playCalcGame = () => {
 };
 
 export default playCalcGame;
-
-
