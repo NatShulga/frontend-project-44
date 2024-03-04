cdac1ffe6608f4bcf74f75d368d3d148a6ecddbb
NatShulga <mrs.flowerfields@gmail.com>
2024-03-04 18:38:49 +0300

    progression.js

--- src/progression.js
+++ src/progression.js
@@ -33,11 +33,9 @@ const playBrainProgression = () => {
   const maxLength = 10;
 
   while (correctAnswers < maxAttempts) {
-    const start =
-      Math.floor(Math.random() * (maxStart - minStart + 1)) + minStart;
+    const start = Math.floor(Math.random() * (maxStart - minStart + 1)) + minStart;
     const diff = Math.floor(Math.random() * (maxDiff - minDiff + 1)) + minDiff;
-    const length =
-      Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+    const length = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
     const hiddenIndex = Math.floor(Math.random() * length);
     const progression = generateProgression(start, diff, length);
     const hiddenProgression = hideElement(progression, hiddenIndex);
@@ -51,9 +49,7 @@ const playBrainProgression = () => {
       console.log("Correct!");
       correctAnswers += 1;
     } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
@@ -61,5 +57,4 @@ const playBrainProgression = () => {
 
   console.log(`Congratulations, ${name}!`);
 };
-
 export default () => { playCalcGame(description, playBrainProgression) };
