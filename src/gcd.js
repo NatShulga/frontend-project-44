5c40c9884ea36398edcc5523066a31ae289be463
NatShulga <mrs.flowerfields@gmail.com>
2024-03-10 17:34:57 +0300

    gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -1,4 +1,4 @@
-import readlineSync from "readline-sync";
+
 import playCalcGame from "./index.js";
 
 const description = "Find the greatest common divisor of given numbers.";
@@ -10,24 +10,11 @@ function gcdGame() {
   while (correctAnswersCount < 3 && !wrongAnswer) {
     const number1 = generateRandomNumber(1, 100);
     const number2 = generateRandomNumber(1, 100);
-    const correctAnswer = getGreatestCommonDivisor(number1, number2);
-    const question = `${number1} ${number2}`;
-    const userAnswer = readlineSync.question(`${question}\nYour answer: `);
-
-    if (Number(userAnswer) === correctAnswer) {
-      console.log("Correct!");
-      correctAnswersCount++;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again!`);
-      wrongAnswer = true;
-    }
-  }
-
-  if (!wrongAnswer) {
-    console.log(`Congratulations!`);
-  }
-}
+    const answer = getGreatestCommonDivisor(number1, number2);
+    const expression = `${number1} ${number2}`;
+    return [expression, String(answer)];
+  };
+};
 const getGreatestCommonDivisor = (number1, number2) => {
   while (number2 !== 0) {
     const temp = number2;
