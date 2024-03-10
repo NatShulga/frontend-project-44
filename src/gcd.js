aaae96711e51cfe68e9dfc9ccc7b810f0ad76e1b
NatShulga <mrs.flowerfields@gmail.com>
2024-03-10 13:16:32 +0300

    gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -1,7 +1,6 @@
+
 import readlineSync from "readline-sync";
-
-import playCalcGame from "./index.js";
-
+import playGcdGame from "./index.js";
 
 const description = "Find the greatest common divisor of given numbers.";
 
@@ -9,30 +8,25 @@ function gcdGame() {
   console.log("Welcome to the Brain Games!");
   const name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
-
   console.log(description);
 
   let correctAnswersCount = 0;
   let wrongAnswer = false;
 
   while (correctAnswersCount < 3 && !wrongAnswer) {
-    const number1 = getRandomNumber();
-    const number2 = getRandomNumber();
+    const number1 = Math.floor(Math.random() * 100) + 1;
+    const number2 = Math.floor(Math.random() * 100) + 1;
     const correctAnswer = getGreatestCommonDivisor(number1, number2);
 
     console.log("What is the result of the expression?");
-    console.log(`Question: ${number1} * ${number2}`);
-    console.log("Your answer:");
-
+    console.log(`Question: ${number1} ${number2}`);
     const userAnswer = readlineSync.question("Your answer: ");
 
     if (Number(userAnswer) === correctAnswer) {
       console.log("Correct!");
       correctAnswersCount++;
     } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       wrongAnswer = true;
     }
@@ -42,20 +36,14 @@ function gcdGame() {
     console.log(`Congratulations, ${name}!`);
   }
 }
-
-const getRandomNumber = () => {
-  return Math.floor(Math.random() * 100) + 1;
-};
-
 const getGreatestCommonDivisor = (number1, number2) => {
   while (number2 !== 0) {
-    const remainder = number1 % number2;
-    number1 = number2;
-    number2 = remainder;
+    [number1, number2] = [number2, number1 % number2];
   }
   return number1;
 };
 
 export default () => {
-  playCalcGame(description, gcdGame);
+  playGcdGame(description, gcdGame);
 };
+
