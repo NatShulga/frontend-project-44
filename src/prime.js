bae354eaf8cc2c5f5368993762fbec044ebbae25
NatShulga <mrs.flowerfields@gmail.com>
2024-03-11 12:22:27 +0300

    prime.js

--- src/prime.js
+++ src/prime.js
@@ -17,7 +17,7 @@ const primeGame = () => {
   console.log("Welcome to the Brain Games!");
   const name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
-  console.log("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+  console.log("Answer \"yes\" if given number is prime. Otherwise answer \"no\".");
 
   const playRound = () => {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
