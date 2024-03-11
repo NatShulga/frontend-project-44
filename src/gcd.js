c2b4bb1505f5cfb38dbb2d402903f6bb54f19db6
NatShulga <mrs.flowerfields@gmail.com>
2024-03-11 10:28:15 +0300

    gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -1,16 +1,15 @@
 
-import playCalcGame from "./index.js";
+//import playCalcGame from "./index.js"; - импорт этой функции подменяет игру
+import readlineSync from "readline-sync";
 
 const description = "Find the greatest common divisor of given numbers.";
 
-const gcdGame = () => {
-    const number1 = generateRandomNumber(1, 100);
-    const number2 = generateRandomNumber(1, 100);
-    const answer = getGreatestCommonDivisor(number1, number2);
-    const expression = `${number1} ${number2}`;
-    return [expression, String(answer)];
-  };
+// Функция для генерации случайного числа в заданном диапазоне
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
+// Функция для вычисления наибольшего общего делителя (НОД) двух чисел
 const getGreatestCommonDivisor = (number1, number2) => {
   while (number2 !== 0) {
     const temp = number2;
@@ -20,6 +19,36 @@ const getGreatestCommonDivisor = (number1, number2) => {
   return number1;
 };
 
-export default () => {
-  playCalcGame(description, gcdGame);
+const gcdGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Find the greatest common divisor of given numbers.');
+
+  let correctAnswersCount = 0;
+  let wrongAnswer = false;
+
+  while (correctAnswersCount < 3 && !wrongAnswer) {
+    const number1 = generateRandomNumber(1, 100);
+    const number2 = generateRandomNumber(1, 100);
+    const correctAnswer = getGreatestCommonDivisor(number1, number2);
+
+    console.log(`Question: ${number1} ${number2}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (Number(userAnswer) === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount++;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      wrongAnswer = true;
+    }
+  }
+
+  if (!wrongAnswer) {
+    console.log(`Congratulations, ${name}!`);
+  }
 };
+
+export default gcdGame;
