6e969e0dfb5781145eeb0e1f456281233324f81e
NatShulga <mrs.flowerfields@gmail.com>
2024-03-12 12:22:06 +0300

    prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,5 @@
 import readlineSync from "readline-sync";
 
-
 const isPrime = (num) => {
   if (num <= 1) {
     return false;
@@ -19,36 +18,27 @@ const primeGame = () => {
   console.log(`Hello, ${name}!`);
   console.log("Answer \"yes\" if given number is prime. Otherwise answer \"no\".");
 
-  const playRound = () => {
+  function askQuestion() {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question("Your answer: ");
-    
-    const correctAnswer = isPrime(randomNumber) ? "yes" : "no";
-    
-    if (userAnswer.toLowerCase() === correctAnswer) {
+    const answer = readlineSync.question("Your answer: ");
+    if (
+      (isPrime(randomNumber) && answer.toLowerCase() === "yes") ||
+      (!isPrime(randomNumber) && answer.toLowerCase() === "no")
+    ) {
       console.log("Correct!");
-      return true;
     } else {
-      console.log(`Incorrect! Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return false;
+      console.log("Incorrect!");
     }
-  };
-
-  let correctAnswersCount = 0;
-  while (correctAnswersCount < 3) {
-    if (!playRound()) {
-      break;
-    }
-    correctAnswersCount++;
+    askQuestion();
   }
 
-  if (correctAnswersCount === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
+  askQuestion();
 };
 
+primeGame();
+
 export default primeGame;
+
 
 
