b3b8420a4b64a6bb95104b2ed59aab45abcfef50
NatShulga <mrs.flowerfields@gmail.com>
2024-03-13 18:26:32 +0300

    calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -7,21 +7,19 @@ const description = 'What is the result of the expression?';
 
 const operators = ['+', '-', '*'];
 
-const calc = () => {
-  
-  const operator = operators[getIndex(0, operators.length - 1)];
-  const number1 = getNumber(1, 20);
-  const number2 = getNumber(1, 20);
-
-  return `${number1} ${operator} ${number2}`;
+const calc = (number1, operator, number2) => {
+  switch (operator) {
+    case '+': return number1 + number2;
+    case '-': return number1 - number2;
+    case '*': return number1 * number2;
+    default: return null;
+  }
 };
 
 const generateRound = () => {
-  const number1 = getNumber(10);
-  const number2 = getNumber(10);
-
-  const chosenOperator = operators[getNumber(3)];
-
+  const number1 = getNumber(0, 10);
+  const number2 = getNumber(0, 10);
+  const chosenOperator = operators[getIndex(operators)];
   const answer = calc(number1, chosenOperator, number2);
   const expression = `${number1} ${chosenOperator} ${number2}`;
   return [expression, String(answer)];
