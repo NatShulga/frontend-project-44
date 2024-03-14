5183693cfe23e66dd4c61a4a80d4cb0dddc43446
NatShulga <mrs.flowerfields@gmail.com>
2024-03-14 15:57:37 +0300

    progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,4 +1,4 @@
-import mainFun from '../index.js;';
+import mainFun from '../index.js';
 
 import { getNumber, getIndex } from "../randomsnum.js";
 
@@ -19,16 +19,12 @@ const generateRound = () => {
   const progressionLength = getNumber(5, 9);
   const progressionStep = getNumber(2, 10);
   const firstNumber = getNumber(1, 100);
-  const progression = getProgression(
-    firstNumber,
-    progressionLength,
-    progressionStep
-  );
-  const randomElement = getIndex(0, progression.length - 1);
-  const answer = progression[randomElement];
-  progression[randomElement] = "..";
-  const expression = progression.join(" ");
-  return [expression, String(answer)];
+  const progression = getProgression(firstNumber, progressionLength, progressionStep);
+  const randomElementIndex = getIndex(0, progression.length - 1);
+  const answerCorrect = String(progression[randomElementIndex]);
+  progression[randomElementIndex] = "..";
+  const question = progression.join(" ");
+  return [question, answerCorrect];
 };
 
 export default () => {
