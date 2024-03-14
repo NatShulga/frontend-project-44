4455b1ae5270195cee031a5edd0c17e457b23925
NatShulga <113417815+NatShulga@users.noreply.github.com>
2024-03-14 18:38:55 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,7 +2,7 @@ import mainFun from '../index.js';
 
 import { getNumber, getIndex } from '../randomsnum.js';
 
-const description = "What number is missing in the progression?"; // арифметическая прогрессия
+const description = 'What number is missing in the progression?'; // арифметическая прогрессия
 
 const getProgression = (startNumber, length, step) => {
   let currentNumber = startNumber;
