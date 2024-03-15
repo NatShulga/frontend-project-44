39614b4f56d08717e96e19864a57ff1e3782a495
NatShulga <113417815+NatShulga@users.noreply.github.com>
2024-03-15 17:04:30 +0300

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,19 +5,19 @@ import { getNumber } from '../utils.js';
 const description = 'Find the greatest common divisor of given numbers.';// наибольший делитель для чисел
 
 const getGcd = (a, b) => {
-if (a < b) return getGcd(b, a);
-if (b === 0) return a;
-return getGcd(b, a % b);
+  if (a < b) return getGcd(b, a);
+  if (b === 0) return a;
+  return getGcd(b, a % b);
 };
 
 const getQuestionAndAnswer = () => {
-const number1 = getNumber(1, 20);
-const number2 = getNumber(1, 20);
+  const number1 = getNumber(1, 20);
+  const number2 = getNumber(1, 20);
 
-const question = `${number1} ${number2}`;
-const result = getGcd(number1, number2).toString();
+  const question = `${number1} ${number2}`;
+  const result = getGcd(number1, number2).toString();
 
-return [question, result];
+  return [question, result];
 };
 
 export default () => {
