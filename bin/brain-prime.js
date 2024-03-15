42ee36cf7f786e4c38bc947aaafa2268f3bfc445
NatShulga <113417815+NatShulga@users.noreply.github.com>
2024-03-15 12:43:27 +0300

    Update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-
-// eslint-disable-next-line max-len
-// eslint-disable-next-line import/extensions, import/no-named-as-default, import/no-named-as-default-member
 import primeGame from '../src/games/prime.js';
 
 primeGame();
