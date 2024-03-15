01fec341d1a190a7c0bd81dccd216974b00a1a4f
NatShulga <113417815+NatShulga@users.noreply.github.com>
2024-03-15 17:09:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const rounds = 3;
 const mainFun = (description, exercise) => {
   console.log('Welcome to the Brain Games!');
 
-  const userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
 
