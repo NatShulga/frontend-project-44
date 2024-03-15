fe837b636c27a554640722afedcbe99e74ef5d52
NatShulga <113417815+NatShulga@users.noreply.github.com>
2024-03-15 16:54:21 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,8 @@
 import readlineSync from 'readline-sync';
 
 export default function greetUser() {
-  // eslint-disable-next-line no-console
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  // eslint-disable-next-line no-console
   console.log(`Hello, ${name}!`);
   return name;
 }
